5c408cefbb5d850a3160f53e0894f6411bcc5b91
liubaige1 <93859686+liubaige1@users.noreply.github.com>
2024-03-02 19:46:29 +0800

    update 03_02

---
+++ test.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+// void test(int &x){
+//     x = 1024;
+//     printf("test函数内部 x = %d\n",x);
+//     printf("test函数内部地址 x = %p\n",&x);
+// }
+int main(){
+    // int sum = 0;
+    // int n=16;
+    // for (int i=1;i<n;i*=2){
+    //     for (int j=0;j<i;j++){
+    //         sum++;
+    //         printf("sum=%d",sum);
+    //     }
+    // }
+    // int x = 0;
+    // int n = 16;
+    // while (n >= (x + 1) * (x + 1))
+    // {
+    //     x += 1;
+    // }
+    // printf("x=%d", x);
+
+    // int x=1023;
+    // printf("调用前 x = %d\n",x);
+    // printf("调用前地址 x = %p\n",&x);
+    // test(x);
+    // printf("调用后 x = %d\n",x);
+    // printf("调用后地址 x = %p\n",&x);
+    return 0;
+}
--- test01.c
+++ test01.c
@@ -1,55 +1,62 @@
-//双链表
+// 双链表
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef int Data;
 typedef struct Node{
-    Data data;//数据域
-    struct Node *prior,*next;//头指针和尾指针
-}DNode,*DNodeList;
+    Data data;                 // 数据域
+    struct Node *prior, *next; // 头指针和尾指针
+} DNode, *DNodeList;
 
-//初始化双向单链表
-void InitDNodeLIst(DNode *L){
-    L=(DNode *)malloc(sizeof(DNode));
-    L->prior=NULL;//头节点的prior始终指向NULL
-    L->next =NULL;
+// 初始化双向单链表
+void InitDNodeLIst(DNode *L)
+{
+    L = (DNode *)malloc(sizeof(DNode));
+    L->prior = NULL; // 头节点的prior始终指向NULL
+    L->next = NULL;
 }
-//在p结点之后插入s结点
-void InsertNextDNode(DNode *p,DNode *s){
-    s->next=p->next;
-    if(p->next!=NULL){
-        s->prior=p;
+// 在p结点之后插入s结点
+void InsertNextDNode(DNode *p, DNode *s)
+{
+    s->next = p->next;
+    if (p->next != NULL)
+    {
+        s->prior = p;
     }
-    p->next->prior=s;
-    p->next=s;
+    p->next->prior = s;
+    p->next = s;
 }
-//删除p结点的后继结点
-void DeleteNextDNote(DNode *p){
-    DNode *q=p->next;
-    if (q!=NULL||q->next!=NULL){
-        p->next=q->next;
+// 删除p结点的后继结点
+void DeleteNextDNote(DNode *p)
+{
+    DNode *q = p->next;
+    if (q != NULL || q->next != NULL)
+    {
+        p->next = q->next;
     }
-    free(q); 
+    free(q);
 }
-//遍历
-//后向遍历
-// while (p!=NULL){
-//     p=p->next;
-// }
-//前向遍历
-// while (p!=NULL){
-//     p=p->prior;
-// }
-//前向遍历,跳过头节点
-// while (p->prior!=NULL){
-//     p=p->prior;
-// }
-void testDLinkList(){
-    //初始化单链表
+// 遍历
+// 后向遍历
+//  while (p!=NULL){
+//      p=p->next;
+//  }
+// 前向遍历
+//  while (p!=NULL){
+//      p=p->prior;
+//  }
+// 前向遍历,跳过头节点
+//  while (p->prior!=NULL){
+//      p=p->prior;
+//  }
+void testDLinkList()
+{
+    // 初始化单链表
     DNodeList L;
     InitDNodeLIst(L);
 }
-int main(){
+int main()
+{
     testDLinkList();
     printf("Hello World\n");
     return 0;
