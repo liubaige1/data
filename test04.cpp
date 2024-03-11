0c35657ce04d8ab08cfb2a5645606feb0b5d7ebd
liubaige1 <93859686+liubaige1@users.noreply.github.com>
2024-03-11 20:05:10 +0800

    update 03_11_01

--- test04.cpp
+++ test04.cpp
@@ -1,70 +1,65 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #define MaxSize 10 //定义栈中元素的最大个数
 typedef int ElemType;
 typedef struct {
     ElemType data[MaxSize]; //静态数组存放栈中元素
-    int top;//栈顶指针
+    int top; //栈顶指针
 } SqStack;
-// InitStack(&S);//初始化栈。构造一个空栈S,分配内存空间。
-// DestroyStack(&L);//销毁栈。销毁并释放栈S所占用的内存空间。
-// Push(&S,x);//进栈，若栈S未满，则将x加入使之成为新栈顶
-// Pop(&S,&x);//出栈，若栈S非空，则弹出栈顶元素，并用x返回。
 
-//初始化栈
-void InitStack(SqStack *S){
-    S->top=-1;
+//初始化栈。构造一个空栈S,分配内存空间。
+void InitStack(SqStack& S) {
+    S.top = -1;
 }
 
 //判断栈是否为空
-bool StackEmpty(SqStack *S){
-    if (S->top==-1){
+bool StackEmpty(SqStack& S) {
+    if (S.top == -1) {
         return true; //栈为空
     }
     return false; //栈不为空
 }
 
-//新元素入栈
-bool Push(SqStack *S,ElemType x){
-    if (S->top==MaxSize-1){
-        return false; 
+//进栈，若栈S未满，则将x加入使之成为新栈顶
+bool Push(SqStack& S, ElemType x) {
+    if (S.top == MaxSize - 1) {
+        return false;
     }
-    S->data[++S->top]=x;
+    S.data[++S.top] = x;
     return true;
 }
 
-//出栈操作
-bool Pop(SqStack *S,ElemType x){
-    if (S->top==-1){
-        return false; 
+//出栈，若栈S非空，则弹出栈顶元素，并用x返回。
+bool Pop(SqStack& S, ElemType x) {
+    if (S.top == -1) {
+        return false;
     }
-    x=S->data[S->top--];//先出栈，指针再减1
+    x = S.data[S.top--]; //先出栈，指针再减1
     return true;
 }
 
 //读栈顶元素
-bool GetTop(SqStack *S,ElemType x){
-    if (S->top==-1){
+bool GetTop(SqStack S, ElemType& x) {
+    if (S.top == -1) {
         return false;
-    } 
-    x=S->data[S->top];//x记录栈顶元素
+    }
+    x = S.data[S.top]; //x记录栈顶元素
     return true;
 }
 
-void testStack(){
+void testStack() {
     SqStack S;
     bool b;
-    int i=0;
-    InitStack(&S);
-    printf("%d\n",StackEmpty(&S));
-    printf("%d\n",Push(&S,i));
-    printf("%d\n",Pop(&S,i));
-
+    int i = 0;
+    InitStack(S);
+    printf("%d\n", StackEmpty(S));
+    printf("%d\n", Push(S, i));
+    printf("%d\n", GetTop(S, i));
+    printf("%d\n", Pop(S, i));
 }
 
-int main(){
+int main() {
     testStack();
     return 0;
-
 }
