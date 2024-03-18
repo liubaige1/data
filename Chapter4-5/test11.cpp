4540bd5975e9dc153ab74dd7bc0bea6f07a40373
liubaige1 <93859686+liubaige1@users.noreply.github.com>
2024-03-18 21:31:17 +0800

    update 03_18

--- Chapter4-5/test11.cpp
+++ Chapter4-5/test11.cpp
@@ -67,4 +67,45 @@ typedef struct StringNode {
         }
         return 0;
     }
+    int index(SString S, SString T) {
+        int i = 1, j = 1;
+        while (i<S.length && j<=T.length) {
+            if (S.ch[i] == T.ch[j]) {
+                i++;
+                j++;
+            } else {
+                i = i-j+2;
+                j = 1;
+            }
+        }
+        if (j>T.length){
+            return i-T.length;
+        }
+        return 0;
+    }
+    int Index_KMP(SString S, SString T, int next[]) {
+        int i = 1, j = 1;
+        while (i < S.length && j <= T.length) {
+            if (j == 0 || S.ch[i] == T.ch[j]) {
+                i++;
+                j++;
+            } else {
+                j = next[j];
+            }
+        }
+        if (j > T.length) {
+            return i - T.length;
+        } else {
+            return 0;
+        }
+    }
+    int netval[100];
+    netval[1] = 0;
+    for (int j = 2; j <= T.length; j++) {
+        if (T.ch[next[j]] == T.ch[j]) {
+            netval[j] = netval[netval[j]];
+        } else {
+            netval[j] = next[j];
+        }
+    }
 */
